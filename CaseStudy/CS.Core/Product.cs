2275955117fee7106dbd640c41ac2cf7be714faa
Jason Bell <jbell@accelebrate.com>
2024-09-24 09:13:01 -0700

    CS2

--- CaseStudy/CS.Core/Product.cs
+++ CaseStudy/CS.Core/Product.cs
@@ -3,4 +3,11 @@
 public class Product
 {
     public int Id { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public double UnitPrice { get; set; }
+    public string? Package { get; set; }
+    public bool IsDicontinued { get; set; }
+
+    public int SupplierId { get; set; }
+    public Supplier? Supplier { get; set; }
 }
--- CaseStudy/CS.Core/Supplier.cs
+++ CaseStudy/CS.Core/Supplier.cs
@@ -2,5 +2,11 @@
 
 public class Supplier
 {
-    //
+    public int Id { get; set; }
+    public string CompanyName { get; set; } = String.Empty;
+    public string? ContactName { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+    public string? Phone { get; set; }
+    public string? Fax { get; set; }
 }
