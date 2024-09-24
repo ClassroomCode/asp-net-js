dc12e3565d72e90ebc6102e1ca317d883662d1e3
Jason Bell <jbell@accelebrate.com>
2024-09-24 06:23:44 -0700

    Deferred execution

--- LangDemo/LangDemo/Program.cs
+++ LangDemo/LangDemo/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var nums = new List<int>() { 1, 2, 3, 4, 5 };
+
+            var evens = nums.Where(i => i % 2 == 0);
+
+            foreach (var n in evens) {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("-------");
+
+            nums.Add(6);
+            foreach (var n in evens) {
+                Console.WriteLine(n);
+            }
         }
     }
 }
