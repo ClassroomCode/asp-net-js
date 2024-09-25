9ee6641b505e68b862bcba49b157c974db553be6
Jason Bell <jbell@accelebrate.com>
2024-09-25 06:04:46 -0700

    Setup for lab

--- CaseStudy/CS.WebUI/wwwroot/js/cart.js
+++ CaseStudy/CS.WebUI/wwwroot/js/cart.js
@@ -1,16 +1,1 @@
-﻿$(document).ready(function () {
-    $('form').submit(function (event) {
-        var formData = {
-            'quantity': $('input[name=quantity]').val()
-        };
-        $.ajax({
-            type: 'POST',
-            url: $('#addToCartForm').attr('action'),
-            data: formData
-        })
-            .done(function (response) {
-                $('#message').html(response);
-            });
-        event.preventDefault();
-    });
-});
+﻿// Write your JavaScript code.
