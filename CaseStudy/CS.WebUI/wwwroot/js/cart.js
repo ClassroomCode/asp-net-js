4e0588b84c8f4951806d3954d50a8063984a9c61
Jason Bell <jbell@accelebrate.com>
2024-09-25 15:56:21 -0700

    Add to cart

--- CaseStudy/CS.WebUI/wwwroot/js/cart.js
+++ CaseStudy/CS.WebUI/wwwroot/js/cart.js
@@ -1,1 +1,19 @@
-﻿// Write your JavaScript code.
+﻿
+const b = document.querySelector("#addToCartButton");
+b.addEventListener("click", addToCart);
+
+async function addToCart() {
+    const q = document.querySelector("#quantity").value;
+    const url = document.querySelector("#addToCartForm").getAttribute("action");
+
+    const data = new FormData();
+    data.append('quantity', q);
+
+    const response = await fetch(url, {
+        method: "POST",
+        body: data
+    });
+    if (response.ok) {
+        document.querySelector("#message").outerHTML = await response.text();
+    }
+}
