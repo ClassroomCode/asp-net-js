925512ef0da7f5b6076a46418cd3ccd016599c40
Jason Bell <jbell@accelebrate.com>
2024-09-26 05:52:42 -0700

    CORS for API

--- CaseStudy/CS.WebAPI/Program.cs
+++ CaseStudy/CS.WebAPI/Program.cs
@@ -17,6 +17,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: "AllowedOrigins",
+        builder => {
+            builder.AllowAnyOrigin();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +31,8 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowedOrigins");
 
 //app.UseAuthorization();
 
